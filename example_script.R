4d1f502f70d71c4e283c6a2a9d1c2478e1cb6b9d
Roy Burstein <rburstein@idmod.org>
2019-07-30 12:12:57 -0700

    adding utils and cleaning example script

--- example_script.R
+++ example_script.R
@@ -22,8 +22,11 @@ family <- 'binomial'
 # 100 good for testing, 1000 good for final run (but slower and more RAM-intensive)
 ndraws <- 100
 
+# outcome variable name
+outcome_varname <- 'dpt'
 
-
+# where your local copy of the generic_mapper repo is (clone from here: https://github.com/rburstein-IDM/generic_mapper)
+codepath  <- 'C:/Users/rburstein/OneDrive - IDMOD/code/generic_mapper'
 
 
 ## ###########################################################
@@ -45,12 +48,9 @@ for(l in libs){
 ## locations - relative to user, but in AMUG Dropbox. Make sure you have access to AMUG to use
 user      <- Sys.info()[['user']]
 root      <- sprintf('C:/Users/%s/Dropbox (IDM)/AMUG/generic_mapper',user)
-temptrash <- sprintf('%s/temptrash',root)
 
-
-## Source the utility functions directly from github repo. 
-script <- getURL("URL HERE", ssl.verifypeer = FALSE)
-eval(parse(text = script))
+# Source the utility functions 
+source(sprintf('%s/utils.R', codepath))
 
 ## Load in data and shapes
 # prepped data
@@ -86,179 +86,41 @@ predfr[, lat := coordinates(ext_raster)[,2]]
 ## ###########################################################
 ## Basic geostats model using INLA (currently with most settings at default)
 
+# get together all the various objects needed for inla to fit the model
+input_obj <- prep_inla_objects(data = d, outcome_varname = outcome_varname, ss_varname = 'N')
 
-# make a wrapper function to do the model  heavy lifting
+  
+# fit inla model
+res_fit <- inla(input_obj[['formula']],
+                data = inla.stack.data(input_obj[['stack']]),
+                control.predictor = list(A       = inla.stack.A(input_obj[['stack']]),
+                                         link    = 1,
+                                         compute = FALSE),
+                control.fixed     = list(expand.factor.strategy = 'inla'),
+                family            = family,
+                num.threads       = 1,
+                Ntrials           = input_obj[['N']],
+                verbose           = TRUE,
+                keep              = FALSE)
 
-fitinlamodel <- function(data = dagg, ndraws = 100, fevars, outcometype){
+summary(res_fit)
   
-  # make design matrix
-  message(' .. data munge')
-  narmdagg      <- na.omit(data)
-  outcome       <- narmdagg[[outcometype]]
-  N             <- narmdagg$N      
-  locs          <- cbind(narmdagg$lon, narmdagg$lat)
-  narmdagg      <- narmdagg[, fevars, with=FALSE]
-  design_matrix <- data.frame(int = 1, narmdagg)
-  
-  # make a mesh
-  mesh <- inla.mesh.2d(loc      = locs,
-                       max.edge = c(.5,5),
-                       offset   = 1,
-                       cutoff   = 0.5)
-  plot(mesh);  points(locs, col = 'red')
-  
-  
-  # make spde object
-  spde  <- inla.spde2.matern(mesh = mesh,  alpha = 2)
-  space <- inla.spde.make.index("space", n.spde = spde$n.spde)
-  
-  
-  # make projector matrix
-  A <- inla.spde.make.A(mesh, loc = locs)
-  
-  
-  # make a data stack
-  stack.obs <- inla.stack(data    = list(o = outcome),
-                          A       = list(A, 1),
-                          effects = list(space, design_matrix),
-                          tag     = 'est')
-  
-  
-  # inla formula
-  formla <- as.formula( sprintf ('o ~ -1 + int + %s + f(space, model = spde)', paste(fevars,collapse='+')) )
-  
-  
-  # fit inla model
-  message(' .. model fit')
-  res_fit <- inla(formla,
-                  data = inla.stack.data(stack.obs),
-                  control.predictor = list(A       = inla.stack.A(stack.obs),
-                                           link    = 1,
-                                           compute = FALSE),
-                  control.compute   = list(dic     = TRUE,
-                                           cpo     = TRUE,
-                                           config  = TRUE),
-                  control.fixed     = list(expand.factor.strategy = 'inla'),
-                  family            = 'binomial', #'gaussian',
-                  num.threads       = 1,
-                  Ntrials           = N,
-                  verbose           = FALSE,
-                  keep              = FALSE,
-                  working.directory = temptrash)
-  
-  summary(res_fit)
-  
-  #########
-  ##  make a prediction surface
-  message(' .. prediction')
-  draws <- inla.posterior.sample(ndraws, res_fit)
-  
-  ## get samples as matrices
-  par_names <- rownames(draws[[1]]$latent)
-  l_idx <- match(res_fit$names.fixed, par_names) # main effects
-  s_idx <- grep('^space.*', par_names) # spatial effects
-  pred_s <- sapply(draws, function (x) x$latent[s_idx])
-  pred_l <- sapply(draws, function (x) x$latent[l_idx])
-  rownames(pred_l) <- res_fit$names.fixed
-  
-  ## if we fit with a nugget, we also need to take draws of the nugget precision
-  #if(length(grep('^IID.ID.*', par_names)) > 0){
-  #  pred_n <- sapply(draws, function(x) {
-  #    nug.idx <- which(grepl('IID.ID', names(draws[[1]]$hyper)))
-  #    x$hyperpar[[nug.idx]]}) ## this gets the precision for the nugget
-  #}else{
-  #  pred_n <- NULL
-  #}
-  
-  coords <- cbind(predfr$lon,predfr$lat)
-  
-  # Projector matrix
-  A.pred <- inla.spde.make.A(mesh = mesh, loc = coords)
-  
-  predfr$int <- 1
-  vals <- predfr[, c('int',fevars), with=FALSE]
-  
-  # get ndraws obs at each predframe location
-  cell_l <- unname(as.matrix(as(data.matrix(vals), "dgeMatrix") %*% pred_l))
-  cell_s <- as.matrix(crossprod(t(A.pred), pred_s))
-  
-  ## add on nugget effect if applicable
-  #if(!is.null(pred_n)){
-  #  cell_n <- sapply(pred_n, function(x){
-  #    rnorm(n = nrow(cell_l), sd = 1 / sqrt(x), mean = 0)
-  #  })
-  #} else
-  
-  # make a cell pred object predframe by ndraws draws
-  message(' .. summarize and save predictions')
-  cell_pred <- plogis(cell_s+cell_l)
-    
-  # make summaries for plotting
-  pred_q     <- rowQuantiles(cell_pred, probs = c(0.025, 0.975))
-  inlapredfr <- data.frame(mean=rowMeans(cell_pred),lower=pred_q[,1],upper=pred_q[,2])
-  predsumr   <- insertRaster(ext_raster,inlapredfr) 
-  
-  
-  return(list(model_object = res_fit,
-              cell_pred    = cell_pred,
-              pred_summary = inlapredfr,
-              raster       = predsumr))
-  
-}
+ 
+
+# do prediction from the fitted model
+pred <- inla_predict(fitted = res_fit, input = input_obj, ndraws = ndraws, predfr = predfr, ext_raster = ext_raster)
+
+
+# the outputs of prediction with match ext_raster. In the following, aggregate them to the inputted shapefile
+agg_res  <- aggregate_results()
 
 
 
 
 
+## ###########################################################
+## Plot some outputs
 
-# fit a separate child and adult model:
-vars <- c('dpt1', 'dpt_dropout', 'type2_sias','mat_edu', 'imp_sani', 'log_u5pop')
-type <- 'type2'
-
-ch_inla_mod <- fitinlamodel(data = dagg[adult==0], fevars = vars, outcometype = type)
-ad_inla_mod <- fitinlamodel(data = dagg[adult==1], fevars = vars, outcometype = type)
-
-
-
-
-## plot it
-hist(ch_inla_mod$pred_summary[,1]) 
-plot(ch_inla_mod$raster) #, zlim = c(0.5, 1.0))
-        
-hist(ad_inla_mod$inlapredfr[,1]) 
-plot(ad_inla_mod$raster) #, zlim = c(0.2, 1.0)) #, main = 'Predicted adult seropositive prevalence')
-
-
-# children minus adult
-plot(ch_inla_mod$raster[[1]]-ad_inla_mod$raster[[1]], main = 'Additional immunity in children over adults')
-         
-diff_cp    <- ch_inla_mod$cell_pred - ad_inla_mod$cell_pred
-diffq      <- rowQuantiles(diff_cp, probs = c(0.025, 0.975))
-diffpredfr <- data.frame(mean=rowMeans(diff_cp),lower=diffq[,1],upper=diffq[,2])
-diff       <- insertRaster(ext_raster,diffpredfr) 
-
-plot(diff[[2:3]]) #, main = 'Predicted adult seropositive prevalence')
-
-
-## Aggregate these results at the district level
-ch <- data.table(ch_inla_mod$cell_pred,adm2=predfr$adm2,pop=predfr$u5pop)
-ad <- data.table(ad_inla_mod$cell_pred,adm2=predfr$adm2,pop=predfr$u5pop)
-df <- data.table(diff_cp,adm2=predfr$adm2,pop=predfr$u5pop)
-
-cols <- paste0('V',1:100)
-ch <- ch[, lapply(.SD, weighted.mean,w=pop,na.rm=TRUE), .SDcols = cols, by=adm2]
-ad <- ad[, lapply(.SD, weighted.mean,w=pop,na.rm=TRUE), .SDcols = cols, by=adm2]
-df <- df[, lapply(.SD, weighted.mean,w=pop,na.rm=TRUE), .SDcols = cols, by=adm2]
-
-
-ch <- ch[,  as.list(quantile(.SD,c(0.025,0.5,0.975),na.rm=TRUE)), by=adm2,.SDcols=cols]
-ad <- ad[,  as.list(quantile(.SD,c(0.025,0.5,0.975),na.rm=TRUE)), by=adm2,.SDcols=cols]
-df <- df[,  as.list(quantile(.SD,c(0.025,0.5,0.975),na.rm=TRUE)), by=adm2,.SDcols=cols]
-names(ch) <- c('id','ch_lower','ch_median','ch_upper')
-names(ad) <- c('id','ad_lower','ad_median','ad_upper')
-names(df) <- c('id','df_lower','df_median','df_upper')
-
-out <- merge(merge(ch, ad, by = 'id', all = TRUE), df, by = 'id', all = TRUE)
 
 
 # shapefile and plot
--- utils.R
+++ utils.R
@@ -12,7 +12,7 @@ message(' <<< LOADING UTILITIES >>> ')
 #' @param new_vals New data matrix, each column representing a layer in the resulting raster brick
 #'
 #' @return a new raster or rasterBrick with the values supplies in new_vals
-insertRaster <- function (raster, new_vals) {
+insert_raster <- function (raster, new_vals) {
   cellIdx <- function (x) 1:ncell(x)
   idx <- cellIdx(raster)
   stopifnot(length(idx) == nrow(new_vals))
@@ -28,6 +28,177 @@ insertRaster <- function (raster, new_vals) {
   }
   return (raster_new)
 }
+
+
+
+
+
+#' Prep all data objects for an INLA geostat model run
+#' Note: Version 1: no covariates, no time, just a GP smoothing model
+#'
+#' @param data data frame where outcome and ss, and coordinates are (later covariates as well)
+#'    make sure xy coordinates are called 'lon' and 'lat'
+#' @param outcome_varname String. Name of outcome variable in data 
+#' @param ss_varname String. Name of sample size variable in data
+#'
+#' @return list obkect of everything we need to run the inla spatial model
+prep_inla_objects <- function(data, outcome_varname, ss_varname){
+  
+  
+  # make a clean design matrix
+  data          <- setDT(data)[,c(outcome_varname,ss_varname,'lon','lat'), with = FALSE]
+  message( sprintf('Original dataset has %i rows.', nrow(data)))
+  data          <- na.rm(data)
+  message( sprintf('NA-removed dataset has %i rows.', nrow(data)))
+  outcome       <- data[[outcome_varname]]
+  N             <- data[[ss_varname]]     
+  locs          <- cbind(data$lon, data$lat)
+  design_matrix <- data.frame(int = rep(1, nrow(data)))
+  
+  # make a mesh
+  # TODO: Allow user to change these args
+  mesh <- inla.mesh.2d(loc      = locs,
+                       max.edge = c(.5,5),
+                       offset   = 1,
+                       cutoff   = 0.5)
+  plot(mesh);  points(locs, col = 'red')
+  
+  
+  # make spde object
+  spde  <- inla.spde2.matern(mesh = mesh,  alpha = 2)
+  space <- inla.spde.make.index("space", n.spde = spde$n.spde)
+  
+  
+  # make projector matrix
+  A <- inla.spde.make.A(mesh, loc = locs)
+  
+  
+  # make a data stack
+  stack.obs <- inla.stack(data    = list(o = outcome),
+                          A       = list(A, 1),
+                          effects = list(space, design_matrix),
+                          tag     = 'est')
+  
+  
+  # inla formula
+  #formla <- as.formula( sprintf ('o ~ -1 + int + %s + f(space, model = spde)', paste(fevars,collapse='+')) )
+  formla <- as.formula( 'o ~ -1 + int + f(space, model = spde)' )
+  
+  # output a list object of everything we need to pass into inla
+  return( list( outcome = outcome,
+                N       = N,
+                dm      = design_matrix,
+                mesh    = mesh,
+                stack   = stack.obs,
+                formula = formla) )
+  
+}
+
+
+
+
+
+
+
+
+
+
+#' Predict surface out of inla model
+#'
+#' @param fitted fitted model object
+#' @param input data inputs that went into inla mode
+#' @param ndraws number of posterior draws to take
+#' @param predfr prediction frame
+#' @param ext_raster prediction raster matching prediction frame
+#'
+#' @return list of three output objects. cell_pred: pixel draws, prediction summary, raster formation prediction summary
+inla_predict <- function(fitted, input, ndraws, predfr, ext_raster){
+  
+
+  ##  get posterior draws for each covariate
+  draws <- inla.posterior.sample(ndraws, fitted)
+  
+  ## get samples as matrices
+  par_names <- rownames(draws[[1]]$latent)
+  l_idx <- match(fitted$names.fixed, par_names) # main effects
+  s_idx <- grep('^space.*', par_names) # spatial effects
+  pred_s <- sapply(draws, function (x) x$latent[s_idx])
+  pred_l <- sapply(draws, function (x) x$latent[l_idx])
+  rownames(pred_l) <- fitted$names.fixed
+  
+  # output coordinates
+  coords <- cbind(predfr$lon,predfr$lat)
+  
+  # Projector matrix from location of REs to prediction frame coordinates
+  A.pred <- inla.spde.make.A(mesh = input[['mesh']], loc = coords)
+  
+  predfr$int <- 1
+  vals <- predfr$int #predfr[, c('int',fevars), with=FALSE]
+  
+  # get ndraws obs at each predframe location
+  cell_l <- unname(as.matrix(as(data.matrix(vals), "dgeMatrix") %*% pred_l))
+  cell_s <- as.matrix(crossprod(t(A.pred), pred_s))
+  
+  # make a cell pred object predframe by ndraws draws
+  message(' .. summarize and save predictions')
+  cell_pred <- plogis(cell_s+cell_l)
+  
+  # make summaries for plotting
+  pred_q     <- rowQuantiles(cell_pred, probs = c(0.025, 0.975))
+  inlapredfr <- data.frame(mean=rowMeans(cell_pred),lower=pred_q[,1],upper=pred_q[,2])
+  predsumr   <- insert_raster(ext_raster,inlapredfr) 
+  
+  
+  return(list(cell_pred    = cell_pred,
+              pred_summary = inlapredfr,
+              raster       = predsumr))
+  
+}
+
+
+
+
+
+
+
+
+
+
+#' Aggregate draws to the adminX level
+#'
+#' @param pred inla_predict
+#' @param predfr prediction frame
+#' @param collapse_to String. Name of column in predfr to collapse to
+#'
+#' @return DT summarizing median, 2.5% and 97.5% quantiles
+aggregate_results <- function(pred, predfr){
+  
+  # make a prediction frame
+  tmp <- data.table(pred$cell_pred, adm=predfr[['collapse_to']], pop=predfr$u5pop)
+  
+  # define draw columns
+  cols <- paste0('V',1:ncols(pred$cell_pred))
+  
+  # collapse each draw, population weighted, to the admin 2
+  tmp  <- tmp[, lapply(.SD, weighted.mean,w=pop,na.rm=TRUE), .SDcols = cols, by=adm]
+  
+  # summarize across draws
+  tmp <- tmp[,  as.list(quantile(.SD,c(0.5,0.025,0.975),na.rm=TRUE)), by=adm, .SDcols=cols]
+  
+  # rename things
+  names(tmp) <- c('id','median','lower','upper')
+  
+  return(tmp)
+  
+}
+
+
+
+
+
+
+
+
 
 
 
